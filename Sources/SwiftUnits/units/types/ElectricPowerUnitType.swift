f2317917e641f4d5f228763fbdbce40c17c5a54a
RandomHashTags <ImRandomHashTags@gmail.com>
2023-10-21 14:05:06 -0500

    added horsepower electric power unit type (but didn't fully add or implement)

--- Sources/SwiftUnits/units/types/ElectricPowerUnitType.swift
+++ Sources/SwiftUnits/units/types/ElectricPowerUnitType.swift
@@ -10,10 +10,20 @@ import Foundation
 public enum ElectricPowerUnitType : String, UnitType {
     public static let category:UnitCategory = UnitCategory.electric_power
     
+    //case horsepower_boiler // TODO: support
+    //case horsepower_electrical
+    //case horsepower_hydraulic
+    //case horsepower_mechanical
+    //case horsepower_metric
     case watt
     
     public var symbol : String {
         switch self {
+        //case .horsepower_boiler: return "hp(S)"
+        //case .horsepower_hydraulic: return "hp(H)"
+        //case .horsepower_electrical: return "hp(E)"
+        //case .horsepower_mechanical: return "hp(I)"
+        //case .horsepower_metric: return "hp(M)"
         case .watt: return "W"
         }
     }
